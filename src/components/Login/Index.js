7d3b671ee983c75932e5922b380500bf8b461fbf
Adam Saparudin <adamsaparudin@gmail.com>
2017-04-17 21:40:01 +0700

    fix error localStorage undefined

--- src/components/Login/Index.js
+++ src/components/Login/Index.js
@@ -16,10 +16,9 @@ class Index extends Component {
   componentWillReceiveProps (nextProps) {
     console.log(nextProps.loggedIn);
     if ((nextProps.loggedIn.token !== '' || nextProps.loggedIn.token !== null) && (nextProps.loggedIn.user !== '' || nextProps.loggedIn.user !== null)) {
-      console.log('firedRedirectLogin');
-      window.localStorage.setItem('token', this.props.loggedIn.token)
-      window.localStorage.setItem('userDetail', JSON.stringify(this.props.loggedIn.user))
-      window.localStorage.setItem('userProfile', JSON.stringify(this.props.loggedIn.data))
+      window.localStorage.setItem('token', nextProps.loggedIn.token)
+      window.localStorage.setItem('userDetail', JSON.stringify(nextProps.loggedIn.user))
+      window.localStorage.setItem('userProfile', JSON.stringify(nextProps.loggedIn.data))
       this.setState({
         redirectLogin: true
       })
