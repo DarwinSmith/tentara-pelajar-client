a5b8ebf9c7084c6251dbe74774ec9074a6d75bdc
didietsuryadi <didietsuryadi>
2017-04-19 15:12:19 +0700

    integrasi

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -90,66 +90,61 @@ class Chat extends React.Component {
                       <a href="javascript:;" className="search"></a>
                   </div>
                   <ul className="people">
-                      <li className="person" data-chat="person1">
-                          <img src="http://s13.postimg.org/ih41k9tqr/img1.jpg" alt="" />
-                          <span className="name">Thomas Bangalter</span>
-                          <span className="time">2:09 PM</span>
-                          <span className="preview">I was wondering...</span>
-                      </li>
-                      <li className="person" data-chat="person2">
-                          <img src="http://s3.postimg.org/yf86x7z1r/img2.jpg" alt="" />
-                          <span className="name">Dog Woofson</span>
-                          <span className="time">1:44 PM</span>
-                          <span className="preview">Ive forgotten how it felt before</span>
-                      </li>
-                      <li className="person" data-chat="person3">
-                          <img src="http://s3.postimg.org/h9q4sm433/img3.jpg" alt="" />
-                          <span className="name">Louis CK</span>
-                          <span className="time">2:09 PM</span>
-                          <span className="preview">But we’re probably gonna need a new carpet.</span>
-                      </li>
-                      <li className="person" data-chat="person4">
-                          <img src="http://s3.postimg.org/quect8isv/img4.jpg" alt="" />
-                          <span className="name">Bo Jackson</span>
-                          <span className="time">2:09 PM</span>
-                          <span className="preview">It’s not that bad...</span>
-                      </li>
-                      <li className="person" data-chat="person5">
-                          <img src="http://s16.postimg.org/ete1l89z5/img5.jpg" alt="" />
-                          <span className="name">Michael Jordan</span>
-                          <span className="time">2:09 PM</span>
-                          <span className="preview">Wasup for the third time like is
-      you bling bitch</span>
-                      </li>
-                      <li className="person" data-chat="person6">
-                          <img src="http://s30.postimg.org/kwi7e42rh/img6.jpg" alt="" />
-                          <span className="name">Drake</span>
-                          <span className="time">2:09 PM</span>
-                          <span className="preview">howdoyoudoaspace</span>
-                      </li>
+                    {
+                      this.state.friendList === ''
+                      ? <img style={{width:"3%"}} src={require("../../assets/image/loading.gif")} alt="Loading" />
+                      : this.state.friendList.map(friend => {
+                        return (
+                          <li key={friend.id} onClick={this.handleFriendChat.bind(this, friend)} className="person" data-chat="person1">
+                              <img src="http://s13.postimg.org/ih41k9tqr/img1.jpg" alt="" />
+                              <span className="name">{friend.fullname}</span>
+                              <span className="time">2:09 PM</span>
+                              <span className="preview">I was wondering...</span>
+                          </li>
+                        )
+                      })
+                    }
                   </ul>
               </div>
               <div className="right">
-                  <div className="top"><span>To: <span className="name">Dog Woofson</span></span></div>
+                  <div className="top"><span>To:
+                  {
+                    this.state.friendChat === ''
+                    ? <span className="name"> Please pick your friend from friend list</span>
+                    : <span className="name">{this.state.friendChat.fullname}</span>
+                  }
+                  </span></div>
                   <div className="chat" data-chat="person1">
                       <div className="conversation-start">
                           <span>Today, 6:48 AM</span>
                       </div>
-                      <div className="bubble you">
-                          Hello,
-                      </div>
-                      <div className="bubble you">
-                          its me.
-                      </div>
-                      <div className="bubble you">
-                          I was wondering...
-                      </div>
+                      {
+                        this.state.chatHistory === ''
+                        ? <p>Start your Chat</p>
+                        : this.state.chatHistory.map(chat => {
+                          if (profile[chat.userId] === null) {
+                            return (
+                              <div key={chat.date} className="bubble you">
+                                  <strong>{this.state.friendChat[chat.userId]}{chat.chat}</strong>
+                                  <span>{chat.date}</span>
+                              </div>
+                            )
+                          } else {
+                            return (
+                              <div key={chat.date} className="bubble me">
+                                  <strong>{profile[chat.userId]}:{chat.chat}</strong>
+                                  <span>{chat.date}</span>
+                              </div>
+                            )
+                          }
+                        })
+                      }
                   </div>
                   <div className="write">
                       <a href="javascript:;" className="write-link attach"></a>
-                      <input type="text" />
+                      <input type="text" name='chat' onChange={this.handleChatChange.bind(this)} />
                       <a href="javascript:;" className="write-link smiley"></a>
-                      <a href="javascript:;" className="write-link send"></a>
+                      <a href="javascript:;" className="write-link send" onClick={this.handleChatData.bind(this)}></a>
                   </div>
               </div>
           </div>
